6f872094d798198954f4defc112b358aa2356172
Denis Kuznetsov <denis.kuznetsov@okkam.ru>
2024-10-25 01:31:28 +0300

    fix

--- src/StorageBundle.php
+++ src/StorageBundle.php
@@ -5,11 +5,18 @@ declare(strict_types=1);
 namespace D3N\StorageBundle;
 
 use D3N\StorageBundle\DependencyInjection\Compiler\StoragePass;
+use D3N\StorageBundle\DependencyInjection\StorageExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class StorageBundle extends Bundle
 {
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new StorageExtension();
+    }
+
     public function build(ContainerBuilder $container): void
     {
         parent::build($container);
